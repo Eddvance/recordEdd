98a79eb8d6de9eeeeaf4c5de93dd5be12b62adb9
Edouard Leroy <leroyedouard@protonmail.com>
2025-02-27 19:25:37 +0100

    architecture MS

---
+++ src/main/java/io/eddvance/production/practice/record_translator/entity/historique/Historique.java
@@ -0,0 +1,40 @@
+package io.eddvance.production.practice.record_translator.entity.historique;
+
+public class Historique {
+
+    private Long histoNumber;
+    private String histoLangue;
+    private String histoDate;
+
+    public Historique() {
+    }
+    public Historique(Long histoNumber, String histoLangue, String histoDate) {
+        this.histoNumber = histoNumber;
+        this.histoLangue = histoLangue;
+        this.histoDate = histoDate;
+    }
+
+    public Long getHistoNumber() {
+        return histoNumber;
+    }
+
+    public void setHistoNumber(Long histoNumber) {
+        this.histoNumber = histoNumber;
+    }
+
+    public String getHistoLangue() {
+        return histoLangue;
+    }
+
+    public void setHistoLangue(String histoLangue) {
+        this.histoLangue = histoLangue;
+    }
+
+    public String getHistoDate() {
+        return histoDate;
+    }
+
+    public void setHistoDate(String histoDate) {
+        this.histoDate = histoDate;
+    }
+}
---
+++ src/test/java/io/eddvance/production/practice/record_translator/HistoriqueApplicationTests.java
@@ -0,0 +1,13 @@
+package io.eddvance.production.practice.record_translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoriqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
